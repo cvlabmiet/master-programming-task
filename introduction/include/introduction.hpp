0a408c74c1c9944321e155424c37ed4c6c730e62
Igor Sharonov <igsha@users.noreply.github.com>
2021-09-04 17:37:20 +0300

    Add introduction lab

---
+++ introduction/include/introduction.hpp
@@ -0,0 +1,35 @@
+/* Introduction class.
+ * @file
+ * @date 2021-09-04
+ * @author Anonymous
+ */
+
+#ifndef __INTRODUCTION_HPP__
+#define __INTRODUCTION_HPP__
+
+#include <string>
+
+//{
+class introduction
+{
+public:
+    std::string_view name() const
+    {
+        return ...
+    }
+
+    std::string_view surename() const
+    {
+        return ...
+    }
+
+    std::string fullname() const
+    {
+        return ...
+    }
+
+private:
+};
+//}
+
+#endif // __INTRODUCTION_HPP__
---
+++ introduction/tests/introduction.cpp
@@ -0,0 +1,22 @@
+/* Test file for introduction task.
+ * @file
+ * @date 2021-09-04
+ * @author Anonymous
+ */
+
+#include <regex>
+
+#include <catch2/catch.hpp>
+
+#include <introduction.hpp>
+
+TEST_CASE("introduction")
+{
+    introduction intr;
+    static_assert(sizeof(intr) > 1);
+
+    CHECK(std::regex_match(intr.name().data(), std::regex{R"([A-Z]\w+)"}));
+    CHECK(std::regex_match(intr.surename().data(), std::regex{R"([A-Z]\w+)"}));
+    CHECK(std::regex_match(intr.fullname(), std::regex{R"(\w+ \w+)"}));
+    // Alternate form is CHECK_THAT(intr.fullname(), Catch::Matchers::Matches(R"(\w+ \w+)"));
+}
