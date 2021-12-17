d63454d7507743695c20d23c5b8ad9424959d047
Igor Sharonov <igsha@users.noreply.github.com>
2021-12-17 10:21:23 +0300

    Update lab8

--- strided_range/include/strided_range.hpp
+++ strided_range/include/strided_range.hpp
@@ -1,11 +1,13 @@
 /* Strided range.
  * @file
- * @date 2019-08-28
+ * @date 2021-12-17
  * @author Anonymous
  */
 
 #ifndef __STRIDED_RANGE_HPP__
 #define __STRIDED_RANGE_HPP__
+
+#include <stdexcept>
 
 #include <boost/range/adaptor/filtered.hpp>
 #include <boost/range/adaptor/indexed.hpp>
@@ -14,6 +16,9 @@
 template<class Range>
 auto make_strided_range(Range& rng, size_t width, size_t stride)
 {
+    if (stride == 0)
+        throw std::invalid_argument("stride != 0");
+
     using namespace boost::adaptors;
     const auto strided = [width, stride](const auto& x) { return x.index() % stride < width; };
     const auto valued = [](const auto& x) { return x.value(); };
--- strided_range/tests/strided_range.cpp
+++ strided_range/tests/strided_range.cpp
@@ -4,46 +4,82 @@
  * @author Anonymous
  */
 
-#include <boost/range/iterator_range.hpp>
 #include <vector>
+
+#include <boost/range/iterator_range.hpp>
+#include <boost/range/irange.hpp>
 
 #include <catch2/catch.hpp>
 
 #include <strided_range.hpp>
 
-// Two checks with different strides.
-TEST_CASE("strided_range::stride")
+//{
+TEST_CASE("strided_range::stride_bigger_width")
 {
+    auto a = boost::irange(0, 8);
+    CHECK(make_strided_range(a, ??, ??) == std::vector{0, 1, 2, 4, 5, 6});
 }
+//}
 
-// Two checks with different widths.
-TEST_CASE("strided_range::width")
+//{
+TEST_CASE("strided_range::width_bigger_stride")
 {
+    auto a = {0, 1, 5, 10, 11, 12, 53};
+    CHECK(make_strided_range(a ???) == ???);
 }
+//}
 
+//{
 TEST_CASE("strided_range::stride_equal_width")
 {
+    CHECK(false); // remove me
 }
+//}
 
-// Use boost::size.
+//{ Use boost::size.
 TEST_CASE("strided_range::distance")
 {
+    CHECK(false); // remove me
 }
+//}
 
-// Make strided_range from another strided_range.
+//{ Make strided_range from another strided_range.
 TEST_CASE("strided_range::from_strided")
 {
+    auto a = {???};
+    auto rng = make_strided_range(a, AAA, AAA + 1);
+    CHECK(make_strided_range(rng, AAA - 1, AAA) == std::vector{???});
 }
+//}
 
-// Provide example with shifted data. It's like a window in the image. Hint: boost::make_iterator_range.
-TEST_CASE("strided_range::shift")
+//{ Remove zeros from range.
+TEST_CASE("strided_range::window")
 {
-}
+    std::vector a{0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0,
+                  0, 1, 2, 3, 4, 0,
+                  0, 5, 6, 7, 8, 0,
+                  0, 9, 10, 11, 12, 0,
+                  0, 13, 14, 15, 16, 0,
+                  0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0};
 
-TEST_CASE("strided_range::corner_case1")
-{
+    auto cropped_range = boost::make_iterator_range(a, ???, ???);
+    auto window = make_strided_range(cropped_range, ??, ??);
+    CHECK(window == boost::irange(1, 17));
 }
+//}
 
-TEST_CASE("strided_range::corner_case2")
+//{
+TEST_CASE("strided_range::corner_case1::zero_width")
 {
+    CHECK(false); // remove me
 }
+//}
+
+//{
+TEST_CASE("strided_range::corner_case2::zero_stride")
+{
+    CHECK(false); // remove me
+}
+//}
